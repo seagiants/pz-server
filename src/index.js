04ed4eba74769998a2276bf94348bc5a717a61df
romaintailhurat <romain.tailhurat@gmail.com>
2017-05-23 09:40:56 +0200

    Adding WebSocket entrypoint

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import express from "express";
+import expressWS from "express-ws";
 
 const app = express();
+// WebSocket capability
+expressWS(app);
 
 app.get("/", (req, res) => {
   res.json({msg:"hello"});
+});
+
+app.get("/newgame", (req, res) => {
+  res.json({
+    gameId: 123456,
+    gameData: {
+      thing: "Full of game data"
+    }
+  });
+});
+
+app.ws("/ws-test", (ws, req) => {
+  ws.on("message", (msg) => {
+    console.log(`[${Date.now()}] Message received: ${msg}`)
+  });
 });
 
 app.listen(9000, () => {
