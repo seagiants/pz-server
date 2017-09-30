97a11c76aacb1134582ad93b728b2364ef9ae69a
romaintailhurat <romain.tailhurat@gmail.com>
2017-09-30 16:47:35 +0200

    Adding a games list endpoint

--- src/index.js
+++ src/index.js
@@ -3,20 +3,18 @@ import expressWS from "express-ws";
 import cors from "cors";
 import { GameBuilder } from "./models/Game";
 
-/* CONSTANTS */
+/* ---------- CONSTANTS */
 const port = 9000;
 
+/* ---------- CONFIG */
 const app = express();
-// CORS, by default allowing everything =)
-app.use(cors());
-// WebSocket capability
+app.use(cors()); // by default allowing everything =)
 expressWS(app);
 
-/* DATA */
+/* ---------- DATA */
 const gameStore = [];
 
-/* ROUTES */
-
+/* ---------- ROUTES */
 // Home
 app.get("/", (req, res) => {
   res.json({ msg: "hello" });
@@ -36,6 +34,11 @@ app.get("/store", (req, res) => {
   res.json(gameStore);
 });
 
+app.get("/games-list", (req, res) => {
+  let gamesId = gameStore.map(game => ({ id: game.id }));
+  res.json(gamesId);
+});
+
 app.get("/game/:id", (req, res) => {
   console.log("[GET] Getting game by id");
   let requestedID = req.params.id;
@@ -47,8 +50,11 @@ app.get("/game/:id", (req, res) => {
 app.ws("/ws-test/:id", (ws, req) => {
   let id = req.params.id;
   // FIXME identify one connection, not a user
-  let clientId = ws.upgradeReq.headers['sec-websocket-key'];
-  console.log(`In ws channel with id ${id}`, `connected user id is ${clientId}`);
+  let clientId = ws.upgradeReq.headers["sec-websocket-key"];
+  console.log(
+    `In ws channel with id ${id}`,
+    `connected user id is ${clientId}`
+  );
   ws.on("message", msg => {
     console.log(`[${Date.now()}] Message received: ${msg}`);
     ws.send("server received message: " + msg);
